14eaadd541984fd707ee7018237300cae4bdb36b
iBoxie <bxgmyt94@gmail.com>
2023-12-22 00:48:49 +0100

    Multiple adds
    An example for personal purposes

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Te dejaré un código muy útil a continuación
+/*  Sirve para hacer que tu elemento seleccionado aparezca con una animación
+    cuando hagas scroll, y llegues a dicho elemento  */
+
+// const image1 = document.getElementById("image1");
+// const image2 = document.getElementById("image2");
+
+// const imageLoad = (entries, observer) => {
+//   entries.forEach((entries) => {
+//     if (entries.isIntersecting) {
+//       entries.target.classList.add("visible");
+//     }
+//   });
+// };
+
+// const observer = new IntersectionObserver(imageLoad, {
+//   root: null,
+//   rootMargin: "0px",
+//   threshold: 0.5,
+// });
+
+// observer.observe(image1);
+// observer.observe(image2);
+
+/* Cuando tenga tiempo, te explico como funciona :D */
